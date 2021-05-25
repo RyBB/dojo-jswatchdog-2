283cfb9ce6d2df34a1a73848978e472bed044fb9
r000909 <r000909@pc-000909-4.local>
2021-05-25 23:27:30 +0900

    [add] allow dateFns

--- config/latest.js
+++ config/latest.js
@@ -1,21 +1,22 @@
 module.exports = {
-  "extends": ["@cybozu", "@cybozu/eslint-config/globals/kintone"],
-  "plugins": ["xss"],
-  "globals": {
-    "garoon": true,
-    "axios": true,
+  extends: ['@cybozu', '@cybozu/eslint-config/globals/kintone'],
+  plugins: ['xss'],
+  globals: {
+    garoon: true,
+    axios: true,
+    dateFns: true,
   },
-  "rules": {
-    "xss/no-mixed-html": 2,
-    "xss/no-location-href-assign": 2,
-    "arrow-parens": 2,
-    "arrow-spacing": 1,
-    "prefer-arrow-callback": 0,
+  rules: {
+    'xss/no-mixed-html': 2,
+    'xss/no-location-href-assign': 2,
+    'arrow-parens': 2,
+    'arrow-spacing': 1,
+    'prefer-arrow-callback': 0,
   },
-  "parserOptions": {
-    "ecmaVersion": 2020
+  parserOptions: {
+    ecmaVersion: 2020,
   },
-  "env": {
-    "es2020": true
-  }
+  env: {
+    es2020: true,
+  },
 };
